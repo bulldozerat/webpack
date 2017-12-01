6283a6aba9b40746d2ea9c46ffa3cfeaff813806
bulldozerat <bulldozerat@gmail.com>
2017-12-01 21:10:54 +0200

    Add files via upload

---
+++ webpack(react+jsx+babel+sass ect)-config/webpack.config.js
@@ -0,0 +1,35 @@
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+module.exports = {
+  entry: './app.js',
+  output: {
+    path: __dirname,
+    filename: './dist/build.js'
+  },
+  //watch: true,
+  devtool: 'source-map',
+  module: {
+  loaders: [
+    {
+      test: /\.scss$/,
+      loader: ExtractTextPlugin.extract({
+      fallback: "style-loader",
+      use: "css-loader?sourceMap!sass-loader?sourceMap"
+      })
+    },
+    {
+      test: /\.js$/,
+      loader: 'babel-loader',
+      exclude: /node_modules$/
+    },
+    {
+      test: /\.jsx$/,
+      loader: 'babel-loader',
+      exclude: /node_modules/ 
+    }
+  ]
+},
+  plugins: [
+    new ExtractTextPlugin("./dist/styles/styles.css"),
+  ]
+};
